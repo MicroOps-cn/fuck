01cee193520a193d0350a5bfffc13e0d007ec2a2
孙林耀 <sunlinyao@vip.qq.com>
2023-03-31 22:12:27 +0800

    Add a limit function. Add test cases for JSONStringer.

--- wrapper/slice.go
+++ wrapper/slice.go
@@ -104,3 +104,32 @@ func (j jsonStringer) String() string {
 func JSONStringer(v any) fmt.Stringer {
 	return &jsonStringer{v: v}
 }
+
+const (
+	PosRight = iota
+	PosLeft
+	PosCenter
+)
+
+func Limit[T any](s []T, limit int, hidePosition int, manySuffix ...T) []T {
+	limit = limit - len(manySuffix)
+	if len(s) > limit {
+		ret := make([]T, 0, limit+len(manySuffix))
+		switch hidePosition {
+		case PosRight:
+			ret = append(ret, s[:limit]...)
+			ret = append(ret, manySuffix...)
+			return ret
+		case PosLeft:
+			ret = append(ret, manySuffix...)
+			ret = append(ret, s[len(s)-limit:]...)
+			return ret
+		case PosCenter:
+			ret = append(ret, s[:limit/2]...)
+			ret = append(ret, manySuffix...)
+			ret = append(ret, s[len(s)-(limit-limit/2):]...)
+			return ret
+		}
+	}
+	return s
+}
--- wrapper/slice_test.go
+++ wrapper/slice_test.go
@@ -185,3 +185,111 @@ func TestFind(t *testing.T) {
 		})
 	}
 }
+
+func TestToInterface(t *testing.T) {
+	ori := []string{"1", "aa", "hello"}
+	if got := Interfaces(ori); len(got) != 3 {
+		t.Errorf("len(Interfaces()) = %v, want %v", len(got), len(ori))
+	}
+}
+
+func TestJSONStringer(t *testing.T) {
+	type args struct {
+		v any
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{{
+		name: "test int",
+		args: args{v: 1},
+		want: "1",
+	}, {
+		name: "test float",
+		args: args{v: 1.99},
+		want: "1.99",
+	}, {
+		name: "test string",
+		args: args{v: "hello"},
+		want: `"hello"`,
+	}, {
+		name: "test struct",
+		args: args{v: struct {
+			FieldA string  `json:"field_a"`
+			FieldB int     `json:"field-b"`
+			FieldC float64 `json:"fieldC"`
+			FieldD []byte
+			fieldE string
+		}{FieldA: "hello", FieldB: 123, FieldC: -22.3, FieldD: []byte("world"), fieldE: "bbb"}},
+		want: `{"field_a":"hello","field-b":123,"fieldC":-22.3,"FieldD":"d29ybGQ="}`,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := JSONStringer(tt.args.v).String(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("JSONStringer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLimit(t *testing.T) {
+	type args struct {
+		s            []byte
+		limit        int
+		manySuffix   []byte
+		hidePosition int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{{
+		name: "Test OK - 1",
+		args: args{s: []byte("hello"), limit: 10},
+		want: []byte("hello"),
+	}, {
+		name: "Test OK - 2",
+		args: args{s: []byte("hello world!!!!!!!!!!!!!"), limit: 10},
+		want: []byte("hello worl"),
+	}, {
+		name: "Test OK - 2",
+		args: args{s: []byte("hello world!!!!!!!!!!!!!"), limit: 10, manySuffix: []byte(" ...")},
+		want: []byte("hello  ..."),
+	}, {
+		name: "Test OK - 3",
+		args: args{s: []byte("helloworld"), limit: 10},
+		want: []byte("helloworld"),
+	}, {
+		name: "Test OK - 4",
+		args: args{
+			s:            []byte(`level=info ts=2022-06-02T01:06:10.469Z caller=tls_config.go:191 msg="TLS is disabled." http2=false`),
+			limit:        20,
+			manySuffix:   []byte(" ... "),
+			hidePosition: PosCenter,
+		},
+		want: []byte("level=i ... p2=false"),
+	}, {
+		name: "Test OK - 4",
+		args: args{
+			s:            []byte(`level=info ts=2022-06-02T01:06:10.469Z caller=tls_config.go:191 msg="TLS is disabled." http2=false`),
+			limit:        20,
+			manySuffix:   []byte(" ... "),
+			hidePosition: PosLeft,
+		},
+		want: []byte(` ... d." http2=false`),
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var oldData []byte
+			oldData = append(oldData, tt.args.s...)
+			got := Limit(tt.args.s, tt.args.limit, tt.args.hidePosition, tt.args.manySuffix...)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Limit() = %v, want %v", string(got), string(tt.want))
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("old data = %v, want %v", string(oldData), string(tt.args.s))
+			}
+		})
+	}
+}
