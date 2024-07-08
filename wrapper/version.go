3d671dbf46ef4e54f18c8ce2f68dba02f1961daf
孙林耀 <sunlinyao@vip.qq.com>
2024-07-08 15:46:54 +0800

    add: version flag
    Signed-off-by: 孙林耀 <sunlinyao@vip.qq.com>

---
+++ wrapper/version.go
@@ -0,0 +1,48 @@
+package w
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	GitCommit string
+	BuildDate string
+	GoVersion string
+	Platform  string
+	Version   string
+)
+
+var timeFormat = []string{
+	time.RFC3339,
+	time.RFC3339Nano,
+	time.DateTime,
+	time.ANSIC,
+	time.UnixDate,
+	time.RubyDate,
+	time.RFC822,
+	time.RFC822Z,
+	time.RFC850,
+	time.RFC1123,
+	time.RFC1123Z,
+	time.RFC822,
+	time.RFC822Z,
+	time.RFC850,
+	time.RFC1123,
+	time.RFC1123Z,
+	"2006-01-02T15:04:05Z0700",
+}
+
+func AddVersionFlags(flagFunc func(shortVersion, fullVersion string)) {
+	for _, tf := range timeFormat {
+		buildDate, err := time.Parse(tf, BuildDate)
+		if err == nil {
+			BuildDate = buildDate.UTC().Format(time.RFC3339)
+			break
+		}
+	}
+	flagFunc(Version, fmt.Sprintf(
+		"%s, build %s/%s [%s/%s]\n",
+		Version, GitCommit, BuildDate, GoVersion, Platform,
+	))
+}
